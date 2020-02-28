821ceb05878247b6ad5b73e0c0af96e1bccf707e
Charles <charles@pop-os.localdomain>
2020-02-28 00:37:28 -0700

    done basically

--- resources/js/my_scripts.js
+++ resources/js/my_scripts.js
@@ -17,92 +17,68 @@ var players = [{name:"John Doe", img: "../resources/img/player1.jpg", alt:"Image
 				{name:"Robert Myers", img: "../resources/img/player4.jpg", alt:"Image of Player 4", year:"Senior", major:"Computer Science", games_played: 31, pass_yards: 802, rushing_yards: 375, receiving_yards: 128}];
 
 
-/*
-	Registration Page:
-		viewStudentStats(id, toggle) method
-			parameters:
-				id - The css id of the html tag being updated.
-				toggle - 
-					0 - hide the html tag
-					1 - make the html tag visible
-			
-			purpose: This method will accept the id of an html tag and a toggle value.
-					 The method will then set the html tag's css visibility and height.  
-					 To hide the html tag (toggle - 0), the visibility will be set to hidden and
-					 the height will be set to 0.  
-					 To reveal the html tag (toggle - 1), the visibility will be set to visible and
-					 the height will be set to auto.
-*/
-				
-/*
-	Home Page: 
-		changeColor(color) method
-			parameter: 
-				color- A css color
-				
-			purpose: This method will set the html body's background color to the 
-					 provided parameter.
-*/
+	function viewStudentStats(id, toggle){	
+		if(toggle == '1'){
+			document.getElementById(id).style.visibility = "visible";
+			document.getElementById(id).style.height = "auto";
+				}
+		else if(toggle == '0'){
+			document.getElementById(id).style.visibility = "hidden";
+			document.getElementById(id).style.height = "0px";
+		}
+	}	
 
+	function changeColor(color){
+	document.body.style.backgroundColor = color;
+	}
+	function loadStatsPage(){
+		var table = document.getElementById("stats_table");
+		var home_value;
+		var away_value;
+		var row_counter;
+		var wins = 0;
+		var loses = 0;
+		for(row_counter = 2; row_counter < table.rows.length; row_counter++){
+			home_value = table.rows[row_counter].cells[2].innerHTML;
+			away_value = table.rows[row_counter].cells[3].innerHTML;
+			home_value = parseInt(home_value);
+			away_value = parseInt(away_value);
+			if(home_value > away_value){
+				table.rows[row_counter].cells[4].innerHTML = 'CU Boulder';	
+				wins++;
+			}
+			else{
+				table.rows[row_counter].cells[4].innerHTML = table.rows[row_counter].cells[1].innerHTML;		
+				loses++;
+			}
+		document.getElementById("wins").innerHTML = wins;
+		document.getElementById("losses").innerHTML = loses;
+		}
+	}
 
-/*
-	Football Season Stats Page:
-		loadStatsPage method:
-			parameters: none
-			
-			purpose: This method will iterate through the stats table and 
-					 do the following:
-						1. Read through each row of the table & determine which team won
-						   the game.
-						
-						2. Update the winner column to the name of the winning team.
-						
-						3. Keep track of the number of wins/losses for the Buffs.
-						
-						4. Update the second table to show the total number of wins/losses for the Buffs.
-*/
-
-/*
-	Football Player Information Page
-		loadPlayersPage method:
-			parameters: none
-			
-			purpose: This method will populate the dropdown menu to allow the 
-					 user to select which player's information to view.
-					 
-					 To handle this, you will need to iterate through the players array
-					 and do the following for each player:
-						1. Create an anchor tag
-						2. Set the href to "#", this will make sure the 
-							anchor tag doesn't change pages
-						3. Set the onclick to call switchPlayers method 
-							(this will need to pass in the index inside the players array)
-						4. Set the anchor tag's text to the player's name.
-						
-					After setting all of the anchor tags, update the innerHTML of the dropdown menu.
-					As a note, the id for the dropdown menu is player_selector.
-		
-		switchPlayers(playerNum) method:
-			parameters: 
-				playerNum - The index of the football player in the players array.
-			
-			purpose:
-				This method will update the the spans on the player's information pageX
-				and calculate the average passing, rushing, and receiving yards.
-				
-				Span ids:
-					p_year     - the player's year in college
-					p_major    - the player's major in college
-					g_played   - the number of games played for Buffs
-					player_img - the player's photo (must set src and alt)
-					p_yards    - the number of passing yards
-					r_yards    - the number of rushing yards
-					rec_yards  - the number of receiving yards
-					
-					Calculated values:
-					  avg_p_yards   - the average number of passing yards for the player's Buff career
-					  avg_r_yards   - the average number of rushing yards for the player's Buff career
-					  avg_rec_yards - the average number of receiving yards for the player's Buff career
-*/
-				
-
+		function loadPlayersPage(){
+			var list = document.getElementById("player_selector")
+			for(let i = 0; i < players.length; i++){
+				var a = document.createElement("a");
+				var text = document.createTextNode("" + players[i].name);
+				a.appendChild(text);
+				a.title = "" + players[i].name;
+				a.href = "#";
+				a.onclick = function(){switchPlayers(i)};
+				list.appendChild(a);
+			}
+			console.log(list)
+		}
+		function switchPlayers(playerNum){
+			let player = players[playerNum];
+			document.getElementById("p_year").innerHTML = player.year
+			document.getElementById("p_major").innerHTML = player.major
+			document.getElementById("g_played").innerHTML = player.games_played
+			document.getElementById("player_img").innerHTML = player.img
+			document.getElementById("p_yards").innerHTML = player.pass_yards
+			document.getElementById("r_yards").innerHTML = player.rushing_yards
+			document.getElementById("rec_yards").innerHTML = player.receiving_yards
+			document.getElementById("avg_p_yards").innerHTML = (player.pass_yards / player.games_played)
+			document.getElementById("avg_r_yards").innerHTML = (player.rushing_yards / player.games_played)
+			document.getElementById("avg_rec_yards").innerHTML = (player.receiving_yards / player.games_played)
+		}
